247e1370190a2c0d891feea1f11d0d5da406f12d
ochosteve <ochosteve08@gmail.com>
2023-04-09 11:23:43 +0100

    create spinner component

---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import spinner from "../assets/spinner.svg";
+const Spinner = () => {
+  return (
+    <div className="bg-black bg-opacity-30 flex justify-center items-center z-50 fixed top-0 bottom-0 left-0 right-0">
+      <img src={spinner} alt="loading.... "  />
+    </div>
+  );
+};
+
+export default Spinner;

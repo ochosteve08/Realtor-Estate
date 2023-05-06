5ef17ef056fb169bbbb73fe70c62bc07110a9e08
ochosteve <ochosteve08@gmail.com>
2023-05-06 13:18:15 +0100

    add offers page

--- src/Pages/Offer.jsx
+++ src/Pages/Offer.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import ListingsItem from "../Components/ListingsItem";
 
 const Offer = () => {
-  return (
-    <div className='text-center'>Offer</div>
-  )
-}
+  const [offerListings, setOfferListings] = useState(null);
 
-export default Offer
+  // for offers
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        const listingsRef = collection(db, "listings");
+        // create the query
+        const q = query(
+          listingsRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc")
+        );
+        const querySnap = await getDocs(q);
+        let listing = [];
+        querySnap.forEach((doc) => {
+          return listing.push({ id: doc.id, data: doc.data() });
+        });
+
+        setOfferListings(listing);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchListing();
+  }, []);
+
+  return (
+    <section className=" max-w-6xl mx-auto px-3 mt-4">
+      <h1 className=" text-3xl text-center font-bold my-6">Offers</h1>
+      <ul className="grid  sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4">
+        {offerListings && offerListings.map((listing) => (
+          <ListingsItem
+            key={listing.id}
+            id={listing.id}
+            listing={listing.data}
+          
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Offer;
